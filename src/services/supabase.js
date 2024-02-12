62430b9d87fff72aadc006a1e9d1aa70761eaf8d
mr-robot-007 <anujgusain108@gmail.com>
2024-02-12 21:37:44 +0530

    4th commit

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = "https://hvtjlffeippxnsjqjjgt.supabase.co";
 // const supabaseKey =
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2dGpsZmZlaXBweG5zanFqamd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ2ODg4NjUsImV4cCI6MjAyMDI2NDg2NX0.vGMhVmH8sVAF1iY1nljjnA924UTbC2KanGA_tK2XEWw";
-const supabaseKey = process.env.SUPABSE_KEY
+const supabaseKey = process.env.SUPABASE_KEY
 // const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
