159e6d3d2722210db324325cc640e0887d5820fa
mr-robot-007 <anujgusain108@gmail.com>
2024-02-15 11:06:42 +0530

    supabase url

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+// export const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+export const supabaseUrl = "https://hvtjlffeippxnsjqjjgt.supabase.co";
+
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 // const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
