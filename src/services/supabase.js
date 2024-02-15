e238cfde416ce7e8343825b40a431b245b473653
mr-robot-007 <anujgusain108@gmail.com>
2024-02-15 11:19:04 +0530

    added env file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-// export const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-export const supabaseUrl = "https://hvtjlffeippxnsjqjjgt.supabase.co";
+export const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+// export const supabaseUrl = process.
 
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 // const supabaseKey = process.env.SUPABASE_KEY;
